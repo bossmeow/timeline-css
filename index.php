b5f08ac358d4dc803a7b62b34720866f2f232e41
ylneoh <being.bosscat@gmail.com>
2016-11-11 12:16:09 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <head>
+
+    <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,700" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css">
+
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <section id="timeline">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12">
+                  <h2>Module</h2>
+                </div> <!-- col-lg-12 -->
+
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 subtabs" data-easytabs="true">
+
+
+
+                        	<ul class="timeline">
+
+                        <li>
+                            <div class="timeline-image">
+                                <h4>A</h4>
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4 class="title">Introduction to Global Business Services :</h4>
+                                    <ul>
+                                    	<li>The history of the shared services, outsourcing and GBS industry</li>
+                                      <li>Why businesses use Global Business Services</li>
+                                      <li>Typical organisational models</li>
+                                      <li>The process typically found in Global Business Services and key decision factors</li>
+                                      <li>The key skills and success factors in a GBS organisation</li>
+                                    </ul>
+                                    <p class="text-muted"><i class="fa fa-clock-o" aria-hidden="true"></i> Duration: 120 Mins</p>
+                                </div>
+
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <h4>B</h4>
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>Process Design, Measurement and Control :</h4>
+                                    <ul>
+                                    	<li>Understanding the process value chain</li>
+                                      <li>Process mapping, SOPs and knowledge transfer</li>
+                                      <li>Governance and process controls</li>
+                                      <li>Process measurement: KPIs and benchmarking </li>
+                                      <li>Current industry trends</li>
+                                    </ul>
+
+                                    <p class="text-muted"><i class="fa fa-clock-o" aria-hidden="true"></i> Duration: 120 Mins</p>
+                                </div>
+
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-image">
+                                <h4>C</h4>
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>Performance Fundamentals :</h4>
+									                           <ul>
+                                    	 <li>Relationship management: Understanding the customer, cultural awareness, communication and behaviour</li>
+                                        <li>The role of technology in Global Business Services </li>
+                                        <li>Understanding compliance and regulation</li>
+                                    </ul>
+								<p class="text-muted"><i class="fa fa-clock-o" aria-hidden="true"></i> Duration: 120 Mins</p>
+                                </div>
+
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <h4>D</h4>
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>Performance Optimisation :</h4>
+                                    <ul>
+                                    	<li>Change management, project management and process improvement</li>
+                                      <li>Personal development, career planning, learning and development</li>
+                                    </ul>
+
+                                    <p class="text-muted"><i class="fa fa-clock-o" aria-hidden="true"></i> Duration: 150 Mins</p>
+                                </div>
+
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-image">
+                                <h4>E</h4>
+                            </div>
+                            <div class="timeline-panel last">
+                                <div class="timeline-heading">
+                                    <h4>Introduction to Finance and Accounting Processes for GBS :</h4>
+                                    <ul>
+                                      <li>Order to cash</li>
+                                      <li>Purchase to pay</li>
+                                      <li>Record to report</li>
+                                      <li>Other processes (FP and A, controlling, payroll, taxation, treasury, supply chain accounting)</li>
+                                    </ul>
+
+                                    <p class="text-muted"><i class="fa fa-clock-o" aria-hidden="true"></i> Duration: 120 Mins</p>
+                                </div>
+
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </div> <!-- col -->
+
+            </div> <!-- row -->
+        </div> <!-- container -->
+
+    </section> <!-- timeline -->
+
+
+
+  </body>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</html>
